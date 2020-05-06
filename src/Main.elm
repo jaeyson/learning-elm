00d203a2b7302cf03200351999f62f047d1bb5e2
jaeyson <hello@jaeyson.ml>
2020-05-06 10:54:41 +0800

    update package.json

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,50 @@
-module Main exposing (addOne, factorial)
+module Main exposing (..)
+
+import Json.Decode as JD
+import Json.Encode as JE
+
+
+json =
+  """
+  [
+    {
+      "role": "admin",
+      "email": "admin@email.com"
+    },
+    {
+      "role": "regular",
+      "email": "user@email.com"
+    },
+    { "email": "test@email.com" },
+    { "other": "foo" }
+  ]
+  """
+
+type User
+  = Admin String
+  | Regular String
+
+regularDecoder = JD.map Regular (JD.field "email" JD.string)
+
+adminDecoder = JD.map Admin (JD.field "email" JD.string)
+
+userType string =
+  case string of
+    "regular" -> regularDecoder
+    "admin" -> adminDecoder
+    _ -> JD.fail <| "err"
+
+userDecoder =
+  JD.field "role" JD.string
+  |> JD.andThen userType
+
+output =
+  JD.decodeString (JD.list userDecoder) json
+
+
+
+---
+
 
 
 addOne : Int -> Int
